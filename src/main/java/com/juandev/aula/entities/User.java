6433e9fe50d595061666758a1b37d5f1ddbcdaf0
Juan <juancmendonca@gmail.com>
2025-01-15 16:15:53 -0300

    /feat: H2, JPA

--- src/main/java/com/juandev/aula/entities/User.java
+++ src/main/java/com/juandev/aula/entities/User.java
@@ -1,12 +1,19 @@
 package com.juandev.aula.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
 
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -75,4 +82,5 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 }
